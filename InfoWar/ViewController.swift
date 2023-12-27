37e536638cad9a4d08b753580c25b1863d687310
Serhat <srhtsimsekk@gmail.com>
2023-12-28 00:01:57 +0300

    Initial Commit

---
+++ InfoWar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfoWar
+//
+//  Created by Serhat  Şimşek  on 28.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
